05587330de8e4a4256f0f8717cb91d5359199195
Samuel Ifere <samifere@yahoo.com>
2016-05-03 15:20:02 +0100

    Modifies classes to correctly talk to each other

--- src/main/java/uk/co/sammy/controller/HomeController.java
+++ src/main/java/uk/co/sammy/controller/HomeController.java
@@ -1,7 +1,16 @@
 package uk.co.sammy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by smlif on 03/05/2016.
  */
+@Controller
 public class HomeController {
+
+    @RequestMapping("/index")
+    public String goHome(){
+        return "index";
+    }
 }
--- src/main/java/uk/co/sammy/controller/ProjectController.java
+++ src/main/java/uk/co/sammy/controller/ProjectController.java
@@ -1,7 +1,37 @@
 package uk.co.sammy.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by smlif on 03/05/2016.
  */
+@Controller
+@RequestMapping("/project")
 public class ProjectController {
+
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String addProject(){
+        System.out.println("Invoking addProject");
+        return "project_add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveProject(){
+        System.out.println("Invoking saveProject");
+        return "project_add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi"})
+    public String saveMultiYearProject(){
+        System.out.println("Invoking saveMultiYearProject");
+        return "project_add";
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST, params = {"type=multi","special"})
+    public String saveSpecialProject(){
+        System.out.println("Invoking saveSpecialProject");
+        return "project_add";
+    }
 }
